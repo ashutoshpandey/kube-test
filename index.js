68f97d4923aaf7fa0289abe5271fe71de4052418
Ashutosh Pandey <ashutoshpandey.in@gmail.com>
2023-06-17 19:09:52 +0530

    Merged deployment and service

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var app = express();
-var fs = require("fs");
 
 app.get('/test', function (req, res) {
-    res.send( {'name': 'Ashutosh Pandey'} );
+    let data = getData();
+    res.send(data);
 });
 
 var server = app.listen(8080, function () {
-   var host = server.address().address;
-   var port = server.address().port;
-   console.log("Example app listening at http://%s:%s", host, port)
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Example app listening at http://%s:%s", host, port)
 });
+
+function getData() {
+    return [{
+        id: 1,
+        name: 'First',
+        gender: 'Male'
+    }, {
+        id: 2,
+        name: 'Second',
+        gender: 'Female'
+    }, {
+        id: 3,
+        name: 'Third',
+        gender: 'Female'
+    }, {
+        id: 4,
+        name: 'Fourth',
+        gender: 'Male'
+    }];
+}
